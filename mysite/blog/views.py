f22fe1dd9e1423e5f0727633b294af349ef5cdf5
RanieryMendes <mendrc18@wfu.edu>
2020-04-12 17:34:12 -0300

    agora vai

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -6,6 +6,7 @@ from django.utils import timezone
 
 
 def post_list(request): 
+    
     posts = Post.objects.filter(publish_date__lte=timezone.now()).order_by('publish_date')
-    return render(request, 'blog/post_list.html', {'posts' : posts})
-   
+    return render(request, 'blog/post_list.html', {'posts' :posts})
+
