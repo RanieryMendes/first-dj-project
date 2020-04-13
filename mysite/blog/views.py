3059b6c1a3c5394440363a2625b45d1a456be80d
RanieryMendes <mendrc18@wfu.edu>
2020-04-12 23:01:04 -0300

    lets

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -5,8 +5,9 @@ from django.utils import timezone
 # Create your views here.
 
 
-def post_list(request): 
-    
-    posts = Post.objects.filter(publish_date__lte=timezone.now()).order_by('publish_date')
-    return render(request, 'blog/post_list.html', {'posts' :posts})
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request, 'blog/post_list.html', {'posts' : posts})
+
+
 
